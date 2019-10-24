e0449b6963b243bdb6f7cdab0dce362c802764cb
Saharak <saharak@bananacoding.com>
2019-10-25 00:57:51 +0700

    [Added] page login

--- src/modules/Auth/Login/LoginView.js
+++ src/modules/Auth/Login/LoginView.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
   StatusBar,
-  View
+  View,
+  Dimensions
 } from 'react-native';
 import {
   Appbar,
@@ -9,17 +10,20 @@ import {
   HelperText,
   TextInput
 } from 'react-native-paper';
+import AnimateLoadingButton from 'react-native-animate-loading-button';
+import I18n from '../../../components/i18n';
+import { showMessage, hideMessage } from 'react-native-flash-message';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 export default class LoginView extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      email: ''
+      email: '',
+      password: ''
     };
-  }
-
-  goBack() {
-    this.props.navigation.goBack();
   }
 
   AppHerder() {
@@ -36,22 +40,59 @@ export default class LoginView extends Component<Props> {
     )
   }
 
+  _onPressHandler() {
+    showMessage({
+      message: 'Hello World',
+      description: 'This is our second message',
+      type: 'success',
+    });
+
+    this.loadingButton.showLoading(true);
+
+    // mock
+    setTimeout(() => {
+      this.loadingButton.showLoading(false);
+    }, 2000);
+  }
+
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         {this.AppHerder()}
-        <TextInput
-          label='Email'
-          mode='outlined'
-          value={this.state.email}
-          onChangeText={email => this.setState({ email: email })}
-        />
-        <HelperText
-          type='error'
-          visible={!this.state.email.includes('@')}
-        >
-          Email address is invalid!
-        </HelperText>
+        <View style={{ padding: 45 }}>
+          <Text style={{ alignItems: 'center', fontSize: 58 }}>Family Plan</Text>
+        </View>
+        <View style={{ padding: 15 }}>
+          <TextInput
+            style={{ paddingBottom: 13 }}
+            label='Email'
+            mode='outlined'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email: email })}
+          />
+
+          <TextInput
+            style={{ paddingBottom: 13 }}
+            label='Password'
+            mode='outlined'
+            value={this.state.password}
+            onChangeText={password => this.setState({ password: password })}
+          />
+
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <AnimateLoadingButton
+              ref={c => (this.loadingButton = c)}
+              width={width - 25}
+              height={50}
+              title={I18n.t('button.signIn')}
+              titleFontSize={16}
+              titleColor='#FFF'
+              backgroundColor='#F7301C'
+              borderRadius={4}
+              onPress={this._onPressHandler.bind(this)}
+            />
+          </View>
+        </View>
       </View>
     );
   }
