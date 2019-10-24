986f50a59ee82f643141d74a1f730dcf59e160f1
Saharak <saharak@bananacoding.com>
2019-10-25 00:57:51 +0700

    [Added] page login

--- src/modules/Home/HomeView.js
+++ src/modules/Home/HomeView.js
@@ -15,11 +15,9 @@ import {
 } from 'react-native-paper';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
-import AnimateLoadingButton from 'react-native-animate-loading-button';
-import { showMessage, hideMessage } from 'react-native-flash-message';
-import InAppBrowser from 'react-native-inappbrowser-reborn'
 import I18n from '../../components/i18n';
 
+// View
 import LoginView from '../Auth/Login/LoginView';
 
 export default class Home extends Component<Props> {
@@ -60,59 +58,11 @@ export default class Home extends Component<Props> {
         <StatusBar backgroundColor='#2370E6' barStyle='light-content' />
         <Appbar.Header style={{ backgroundColor: '#2370E6' }}>
           <Appbar.Content
-            title='Home'
+            title='Family Plan'
           />
         </Appbar.Header>
       </View>
     )
-  }
-
-  _onPressHandler() {
-    this.openLink()
-    showMessage({
-      message: 'Hello World',
-      description: 'This is our second message',
-      type: 'success',
-    });
-
-    this.loadingButton.showLoading(true);
-
-    // mock
-    setTimeout(() => {
-      this.loadingButton.showLoading(false);
-    }, 2000);
-  }
-
-  async openLink() {
-    try {
-      const url = 'https://www.google.com'
-      if (await InAppBrowser.isAvailable()) {
-        const result = await InAppBrowser.open(url, {
-          dismissButtonStyle: 'cancel',
-          preferredBarTintColor: '#2370E6',
-          preferredControlTintColor: 'white',
-          readerMode: false,
-          modalEnabled: true,
-          showTitle: true,
-          toolbarColor: '#2370E6',
-          secondaryToolbarColor: 'black',
-          enableUrlBarHiding: true,
-          enableDefaultShare: true,
-          forceCloseOnRedirection: false,
-          animations: {
-            startEnter: 'slide_in_right',
-            startExit: 'slide_out_left',
-            endEnter: 'slide_in_left',
-            endExit: 'slide_out_right'
-          },
-          waitForRedirectDelay: 0
-        })
-        Alert.alert(JSON.stringify(result))
-      }
-      else Linking.openURL(url)
-    } catch (error) {
-      Alert.alert(error.message)
-    }
   }
 
   render() {
@@ -127,20 +77,8 @@ export default class Home extends Component<Props> {
           />
         </View>
 
-        <View style={{ flex: 1, backgroundColor: 'rgb(255,255,255)', justifyContent: 'center' }}>
-          <AnimateLoadingButton
-            ref={c => (this.loadingButton = c)}
-            width={300}
-            height={50}
-            title={I18n.t('button.signIn')}
-            titleFontSize={16}
-            titleColor='rgb(255,255,255)'
-            backgroundColor='rgb(29,18,121)'
-            borderRadius={4}
-            onPress={this._onPressHandler.bind(this)}
-          />
-        </View>
         {this.logInModal()}
+
         <ActionButton
           buttonColor='rgba(231,76,60,1)'
           onPress={() => {
