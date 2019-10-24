7e912b6716f9ccb6e33f2607ef5894ee5f3f3856
Saharak <saharak@bananacoding.com>
2019-10-25 02:00:01 +0700

    [Setup] code

--- src/modules/Home/HomeView.js
+++ src/modules/Home/HomeView.js
@@ -39,6 +39,9 @@ export default class Home extends Component<Props> {
         animationType='slide'
         transparent={false}
         visible={this.state.modalVisible}
+        onRequestClose={() => {
+          this.setState({ modalVisible: false });
+        }}
       >
         <LoginView
           back={
