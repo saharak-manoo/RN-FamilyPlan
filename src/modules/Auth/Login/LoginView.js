36fedbfcf520d50279474728edafc928e25bfab3
Saharak <saharak@bananacoding.com>
2019-10-25 02:00:01 +0700

    [Setup] code

--- src/modules/Auth/Login/LoginView.js
+++ src/modules/Auth/Login/LoginView.js
@@ -69,6 +69,9 @@ export default class LoginView extends Component<Props> {
         animationType='slide'
         transparent={false}
         visible={this.state.forgotPasswordModal}
+        onRequestClose={() => {
+          this.setState({ forgotPasswordModal: false });
+        }}
       >
         <ForgotPasswordView
           back={
