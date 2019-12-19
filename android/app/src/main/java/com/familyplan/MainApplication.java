f73047489d0ebf15a5417c1ef8e19966092fb652
Saharak <Saharakmanoo@outlook.com>
2019-12-19 11:00:36 +0700

    [Added] sign in with button

--- android/app/src/main/java/com/familyplan/MainApplication.java
+++ android/app/src/main/java/com/familyplan/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.henninghall.date_picker.DatePickerPackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
