8d94e0697005638c7a24da0629251e10dee9b3b0
Saharak <Saharakmanoo@outlook.com>
2020-01-02 14:46:59 +0700

    [Added] request permission noti

--- src/modules/Modal/SetUpReminderView.js
+++ src/modules/Modal/SetUpReminderView.js
@@ -14,60 +14,73 @@ const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 export default class SetUpReminderView extends Component<Props> {
-	constructor(props) {
-		super(props);
-		this.state = {
-			dueDate: GFunction.strToDate(this.props.group.due_date) || new Date(),
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      dueDate: GFunction.strToDate(this.props.group.due_date) || new Date(),
+    };
+  }
 
-	async clickSettingDueDate() {
-		this.loadingSettingDueDate.showLoading(true);
-		let user = await GFunction.user();
-		let params = {
-			due_date: this.state.dueDate,
-		};
+  async clickSettingDueDate() {
+    this.loadingSettingDueDate.showLoading(true);
+    let user = await GFunction.user();
+    let params = {
+      due_date: this.state.dueDate,
+    };
 
-		let response = await Api.updateGroup(user.authentication_jwt, this.props.group.id, params);
+    let response = await Api.updateGroup(
+      user.authentication_jwt,
+      this.props.group.id,
+      params,
+    );
 
-		if (response.success) {
-			this.loadingSettingDueDate.showLoading(false);
-			GFunction.successMessage(I18n.t('message.success'), I18n.t('message.settingDueDateSuccessful'));
-			this.props.modal.current.close();
-			this.props.group.due_date = GFunction.dateToStr(this.state.dueDate);
-			this.props.onSetNewData(this.props.group);
-		} else {
-			this.loadingSettingDueDate.showLoading(false);
-			let errors = [];
-			response.error.map((error, i) => {
-				errors.splice(i, 0, I18n.t(`message.${GFunction.camelize(error)}`));
-			});
-			GFunction.errorMessage(I18n.t('message.notValidate'), errors.join('\n'));
-		}
-	}
+    if (response.success) {
+      this.loadingSettingDueDate.showLoading(false);
+      GFunction.successMessage(
+        I18n.t('message.success'),
+        I18n.t('message.settingDueDateSuccessful'),
+      );
+      this.props.modal.current.close();
+      this.props.group.due_date = GFunction.dateToStr(this.state.dueDate);
+      this.props.onSetNewData(this.props.group);
+    } else {
+      this.loadingSettingDueDate.showLoading(false);
+      let errors = [];
+      response.error.map((error, i) => {
+        errors.splice(i, 0, I18n.t(`message.${GFunction.camelize(error)}`));
+      });
+      GFunction.errorMessage(I18n.t('message.notValidate'), errors.join('\n'));
+    }
+  }
 
-	render() {
-		return (
-			<View style={{flex: 1, padding: 30}}>
-				<Text style={{fontSize: 30}}>{I18n.t('placeholder.setUpAReminder')}</Text>
-				<View style={{paddingTop: 15}}>
-					<DatePicker date={this.state.dueDate} mode="date" onDateChange={dueDate => this.setState({dueDate})} />
-				</View>
+  render() {
+    return (
+      <View style={{flex: 1, padding: 30}}>
+        <Text style={{fontSize: 30}}>
+          {I18n.t('placeholder.setUpAReminder')}
+        </Text>
+        <View style={{flex: 1, paddingTop: 15}}>
+          <DatePicker
+            date={this.state.dueDate}
+            mode="date"
+            onDateChange={dueDate => this.setState({dueDate})}
+          />
+        </View>
 
-				<View style={{paddingTop: 35}}>
-					<AnimateLoadingButton
-						ref={c => (this.loadingSettingDueDate = c)}
-						width={width - 25}
-						height={50}
-						title={I18n.t('button.submit')}
-						titleFontSize={18}
-						titleColor="#FFF"
-						backgroundColor="#03C8A1"
-						borderRadius={25}
-						onPress={this.clickSettingDueDate.bind(this)}
-					/>
-				</View>
-			</View>
-		);
-	}
+        <View style={{paddingTop: 35}}>
+          <AnimateLoadingButton
+            ref={c => (this.loadingSettingDueDate = c)}
+            width={width - 25}
+            height={50}
+            title={I18n.t('button.submit')}
+            titleFontSize={18}
+            titleColor="#FFF"
+            backgroundColor="#03C8A1"
+            borderRadius={25}
+            onPress={this.clickSettingDueDate.bind(this)}
+          />
+        </View>
+      </View>
+    );
+  }
 }
