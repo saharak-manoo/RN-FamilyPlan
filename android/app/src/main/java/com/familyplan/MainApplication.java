5bdef62f97de7c45a9ca4807346d53f726dd1628
Saharak <Saharakmanoo@outlook.com>
2020-01-16 20:23:04 +0700

    [Added] face ID and touch ID

--- android/app/src/main/java/com/familyplan/MainApplication.java
+++ android/app/src/main/java/com/familyplan/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
 import com.codemotionapps.reactnativedarkmode.DarkModePackage;
 import com.horcrux.svg.SvgPackage;
 import io.invertase.firebase.RNFirebasePackage;
